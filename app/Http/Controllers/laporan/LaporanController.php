553186dd4c78c44e57201993f37b738f6e288cae
Diyas Dwiya <diyasdwiya@gmail.com>
2022-10-10 23:08:01 +0700

    update all

--- app/Http/Controllers/laporan/LaporanController.php
+++ app/Http/Controllers/laporan/LaporanController.php
@@ -11,10 +11,10 @@ class LaporanController extends Controller
   public function index()
   {
     $booking = Booking::select('booking.id_booking','booking.id_product','booking.price_product',
-      'booking.id_subsidi','booking.subsidi_value','booking.booking_type','booking.payment_status','booking.created_at','bt.type_name',
-      'cicilan_value','booking.booking_total','booking.booking_status','bg.title','bg.fullname','p.product_name','s.subsidi_type_name')
+      'booking.id_subsidi','booking.subsidi_value','booking.booking_type','booking.payment_status','booking.created_at','bt.name as type_name',
+      'cicilan_value','booking.booking_total','booking.booking_status','bg.fullname','p.product_name','s.subsidi_type_name')
       ->leftjoin('booking_guest as bg','bg.id_booking','=','booking.id_booking')
-      ->leftjoin('booking_type as bt','bt.id','=','booking.booking_type')
+      ->leftjoin('parameter_etail as bt','bt.id','=','booking.booking_type')
       ->leftjoin('product as p','p.product_id','=','booking.id_product')
       ->leftjoin('master_subsidi_type as s','s.id','=','booking.id_subsidi')
       ->paginate(10);
